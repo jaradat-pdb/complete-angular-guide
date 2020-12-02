55b1d32fff0e47aaeab5a2617012536edebb26d6
jaradat-pdb <admin@pdbcorp.org>
2020-12-02 01:29:23 -0500

    Add completed assignment 003 codebase

--- 003/practicing-directives/src/app/details/details.component.ts
+++ 003/practicing-directives/src/app/details/details.component.ts
@@ -5,14 +5,24 @@ import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent implements OnInit, OnChanges {
+export class DetailsComponent implements OnInit {
+  static stylingIndex: number = 5;
   revealSecret: boolean = false;
+  events: string[] = [];
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.revealSecret = !this.revealSecret;
+  isRevealSecret(): boolean {
+    return this.revealSecret = !this.revealSecret;
+  }
+
+  applyStyling(i: number): boolean {
+    return (i >= DetailsComponent.stylingIndex);
+  }
+
+  logEvent(eventType: string): void {
+    this.events.push(`${eventType} event occurred at ${new Date()}`);
   }
 }
