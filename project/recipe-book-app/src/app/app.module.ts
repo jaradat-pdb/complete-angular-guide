f26f2c4114d0b96bbfccfbd709395ba1f47d0144
jaradat-pdb <admin@pdbcorp.org>
2020-12-27 16:52:12 -0500

    Work in dev

--- project/recipe-book-app/src/app/app.module.ts
+++ project/recipe-book-app/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { FrameModule } from './frame/frame.module';
 import { SharedModule } from './shared/shared.module';
 
+import { RecipeModule } from './main/recipe/recipe.module';
+import { TodoModule } from './main/todo/todo.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -15,7 +17,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
     SharedModule,
-    FrameModule
+    FrameModule,
+    RecipeModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [
