2869a40ba92f596517c0879935602bb692bde4ec
waljara <waljaradat@uhc.com>
2021-01-01 23:52:53 -0500

    Add two sections to html template, one for listing and one for adding todos, along with styling

--- project/recipe-book-app/src/app/main/todo/todo-list/todo-list.component.ts
+++ project/recipe-book-app/src/app/main/todo/todo-list/todo-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Todo } from '../todo.model';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  todos: Todo[] = [
+    new Todo('Ex Todo', 'Example of content of a todo.')
+  ];
 
   constructor() {}
 
