bdc8aacdf1319a0f8b243d837699855e29c1cefa
waljara <waljaradat@uhc.com>
2021-01-02 08:16:26 -0500

    Made main log method private and exposed multiple implementations to enforce uniform formatting of log messages

--- project/recipe-book-app/src/app/shared/services/logger/logger.service.ts
+++ project/recipe-book-app/src/app/shared/services/logger/logger.service.ts
@@ -7,8 +7,39 @@ export class LoggerService {
 
   constructor() {}
 
-  log(lvl: string, msg: string): void {
-    console.log(`${lvl} | ${new Date().toISOString()} | ${msg}`);
+  logConstruction(msg: string): void {
+    this.traceLog(`Constructing ${msg}`);
   }
 
+  logInitialization(msg: string): void {
+    this.traceLog(`Initializing ${msg}`);
+  }
+
+  traceLog(msg: string): void {
+    this.log('TRACE', msg);
+  }
+
+  debugLog(msg: string): void {
+    this.log('DEBUG', msg);
+  }
+
+  infoLog(msg: string): void {
+    this.log('INFO ', msg);
+  }
+
+  warnLog(msg: string): void {
+    this.log('WARN ', msg);
+  }
+
+  errorLog(msg: string): void {
+    this.log('ERROR', msg);
+  }
+
+  fatalLog(msg: string): void {
+    this.log('FATAL', msg);
+  }
+
+  private log(lvl: string, msg: string): void {
+    console.log(`${lvl} | ${new Date().toISOString()} | ${msg}`);
+  }
 }
